9f3f7bec6f7c259fc4899d353a9faa2ddc6f8fa7
esusaki <susaki2002@gmail.com>
2024-07-03 11:55:56 +0900

    feat: create login ui

--- src/app/(pages)/auth/login/page.tsx
+++ src/app/(pages)/auth/login/page.tsx
@@ -1,9 +1,12 @@
 import SiginInWithGoogleBtn from "@/app/components/auth/signInWithGoogleBtn";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <div>
-        <SiginInWithGoogleBtn />
+    <div className="flex flex-col items-center pt-[100px]">
+      <div className="text-[30px] text-center font-bold">Sign In To Portolio</div>
+      <Image src="/images/login/夕焼けを見て思索に耽る人.png" alt="夕焼けを見て思索に耽る人のイラスト" width={270} height={270} className="mb-3"/>
+      <SiginInWithGoogleBtn />
     </div>
   )
 }
