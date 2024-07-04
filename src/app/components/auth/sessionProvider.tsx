da7663023535aef003593756e1ba181e558171a8
balckowl <shiggyjr1492@gmail.com>
2024-07-04 14:11:58 +0900

    chore: restore darkmode

--- src/app/components/auth/sessionProvider.tsx
+++ src/app/components/auth/sessionProvider.tsx
@@ -1,12 +1,20 @@
 "use client"
 
+import { ThemeProvider } from "@/components/theme-provider"
 import { SessionProvider as Provider } from "next-auth/react"
 import { ReactNode } from "react"
 
 export default function SessionProvider({ children }: { children: ReactNode }) {
     return (
         <div>
-            <Provider>{children}</Provider>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                <Provider>{children}</Provider>
+            </ThemeProvider>
         </div>
     )
 }
